9e306888655b5930ae7b3f2610fe0bd2f5931c79
Jonathan Zhang <jz2814@columbia.edu>
2016-10-16 23:01:12 -0400

    some minor formatting + record of how to use MTA API

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def test():
-	return "TESTING TESTING TESTING"
-	
+    return "TESTING TESTING TESTING"
--- getFeedsTEMP.py
+++ getFeedsTEMP.py
@@ -5,10 +5,12 @@ from API_KEY import key
 
 # demonstrates how to use FeedMessages to get data from API
 
-raw_gtfs = urllib2.urlopen("http://datamine.mta.info/mta_esi.php?key=" + key + "&feed_id=1")
+raw_gtfs = urllib2.urlopen("http://datamine.mta.info/mta_esi.php?key=" +
+                           key + "&feed_id=1")
 feed = gtfs.FeedMessage()
 feed.ParseFromString(raw_gtfs.read())
 
+# testing API usage
 for entity in feed.entity:
-	print entity
-
+    if (entity.trip_update.trip.HasExtension(nyct.nyct_trip_descriptor)):
+        print entity.trip_update.trip.Extensions[nyct.nyct_trip_descriptor]
