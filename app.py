621c12997fb1974357f44472819b1c1479311e4c
Jonathan Zhang <jz2814@columbia.edu>
2016-10-22 20:13:09 -0400

    more testing, webapp functionality, static files

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import getFeedsTEMP
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def test():
-    return "TESTING TESTING TESTING"
+def index():
+    entities = []
+    for entity in getFeedsTEMP.feed.entity:
+        route_id = entity.trip_update.trip.route_id
+        vehicle_id = entity.vehicle.trip.route_id
+        if ((route_id != "" and route_id == "5") or
+           (vehicle_id != "" and vehicle_id == "5")):
+            entities.append(entity)
+
+    return render_template("index.html", entities=entities)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- getFeedsTEMP.py
+++ getFeedsTEMP.py
@@ -1,16 +1,20 @@
 import urllib2
+import requests
 import gtfs_realtime_pb2 as gtfs
 import nyct_subway_pb2 as nyct
 from API_KEY import key
 
 # demonstrates how to use FeedMessages to get data from API
 
-raw_gtfs = urllib2.urlopen("http://datamine.mta.info/mta_esi.php?key=" +
-                           key + "&feed_id=1")
+# raw_gtfs = urllib2.urlopen("http://datamine.mta.info/mta_esi.php?key=" +
+#                           key + "&feed_id=1")
+
+raw_gtfs = requests.get("http://datamine.mta.info/mta_esi.php?key=" +
+                        key + "&feed_id=1")
 feed = gtfs.FeedMessage()
-feed.ParseFromString(raw_gtfs.read())
+feed.ParseFromString(raw_gtfs.content)
 
 # testing API usage
-for entity in feed.entity:
-    if (entity.trip_update.trip.HasExtension(nyct.nyct_trip_descriptor)):
-        print entity.trip_update.trip.Extensions[nyct.nyct_trip_descriptor]
+# for entity in feed.entity:
+#     if (entity.trip_update.trip.HasExtension(nyct.nyct_trip_descriptor)):
+#         print entity.trip_update.trip.Extensions[nyct.nyct_trip_descriptor]
