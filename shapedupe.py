22b65481a1076eeb41abf8a17473a704f38aa8f6
Garrison <garrison.grogan@columbia.edu>
2016-12-02 16:55:59 -0500

    python conventions

--- shapedupe.py
+++ shapedupe.py
@@ -3,17 +3,16 @@ i = 0
 points = []
 truelines = []
 for lines in shapes:
-	if i!=0:
-		xtemp = lines.split(',')[1]
-		ytemp = lines.split(',')[2]
-		if  xtemp+""+ytemp not in points:
-			points.append(xtemp+""+ytemp)
-			truelines.append(lines)
-	else:
-		truelines.append(lines)	
-
-	i=i+1
-newfile = open("static_transit/newshapes.txt",'w+')
+    if i != 0:
+        xtemp = lines.split(',')[1]
+        ytemp = lines.split(',')[2]
+        if  xtemp + "" + ytemp not in points:
+            points.append(xtemp + "" + ytemp)
+            truelines.append(lines)
+    else:
+        truelines.append(lines) 
+    i = i + 1
+newfile = open("static_transit/newshapes.txt", 'w+')
 for line in truelines:
-	newfile.write(line)
+    newfile.write(line)
 newfile.close()
