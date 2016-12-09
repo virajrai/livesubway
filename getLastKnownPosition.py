7843e2ceecc9dc6b9708c79f72eaf36172d8f6e7
virajrai <virajrai97@gmail.com>
2016-12-09 14:58:22 -0500

    Added last known location script + train data node module

---
+++ getLastKnownPosition.py
@@ -0,0 +1,85 @@
+import urllib2
+import gtfs_realtime_pb2 as gtfs
+import nyct_subway_pb2 as nyct
+from API_KEY import key
+import pygtfs
+
+# demonstrates how to use FeedMessages to get data from API
+
+raw_gtfs = urllib2.urlopen("http://datamine.mta.info/mta_esi.php?key=" + key + "&feed_id=1")
+feed = gtfs.FeedMessage()
+feed.ParseFromString(raw_gtfs.read())
+directionDict = {}
+directionDict[1]="North"
+directionDict[3]="South"
+
+
+def getUpdate():
+     return (feed.entity)
+
+
+def call():
+
+    dict={}
+    i=0
+    done = False;
+    for ii in range(len(feed.entity)):
+        entity = feed.entity[ii]
+        i+=1
+        if (done):
+            break;
+        if entity.trip_update.trip.HasExtension(nyct.nyct_trip_descriptor):
+            if (entity.trip_update.trip.Extensions[nyct.nyct_trip_descriptor].is_assigned):
+                print(entity)
+                print(feed.entity[ii+1])
+                entity2 = feed.entity[ii+1]
+                stops=[]
+
+                print "Trip id:" ,str(entity.trip_update.trip.trip_id)
+                print "Route id:" , entity.trip_update.trip.route_id
+                print "Current stop number:", entity2.vehicle.current_stop_sequence
+                print "Current status:", entity2.vehicle.current_status
+                for stop in entity.trip_update.stop_time_update:
+                    stops.append((stop.stop_id, stop.arrival.time))
+
+                if entity2.vehicle.current_status== 1:
+                    last_known = stops[0]
+                else:
+                    last_known= None
+                print "Last Known Stop:",last_known
+
+                print(stops)
+                break
+                """"
+                haha = None
+                for stop in entity.trip_update.stop_time_update:
+
+                    if stop.HasExtension(nyct.nyct_stop_time_update):
+                        if hasattr(stop.Extensions[nyct.nyct_stop_time_update],'actual_track'):
+                            if (stop.Extensions[nyct.nyct_stop_time_update].actual_track) !='':
+                                print(entity)
+                                if(i==2):
+                                    done = True
+                                break;
+                                haha = stop
+                if haha!=None:
+                    if (haha.Extensions[nyct.nyct_stop_time_update].actual_track == '1'):
+                        print(haha)
+                        print(directionDict[entity.trip_update.trip.Extensions[nyct.nyct_trip_descriptor].direction])
+                        """
+
+
+call()
+
+#print(feed)
+
+
+
+#Incoming at = 0
+#Stopped = 1
+#In-transit = 2
+
+
+
+
+
---
+++ train_schedule_node.py
@@ -0,0 +1,55 @@
+class train_schedule_node(object):
+    trip_id = None
+    last_known_stop = None
+    last_known_time = None
+    stop_number = None
+    scheduled_stops = []
+    route_id = None
+    target_stop = None
+    target_stop_time = None
+
+    def __init__(self, trip_id, route_id, stop_number, current_status, scheduled_stops):
+        self.trip_id = trip_id
+        self.stop_number = stop_number
+        self.scheduled_stops = scheduled_stops
+        self.route_id = route_id
+        if (current_status <= 1):
+            self.last_known_stop = scheduled_stops[0][0]
+            self.last_known_time = scheduled_stops[0][1]
+            if (len(scheduled_stops) > 1):
+                self.target_stop = scheduled_stops[1][0]
+                self.target_stop_time = scheduled_stops[1][1]
+        else:
+            self.target_stop = scheduled_stops[0][0]
+            self.target_stop_time = scheduled_stops[0][1]
+
+    def update(self, stop_number, current_status, scheduled_stops):
+        self.stop_number = stop_number
+        self.scheduled_stops = scheduled_stops
+        if (current_status <= 1):
+            self.last_known_stop = scheduled_stops[0][0]
+            self.last_known_time = scheduled_stops[0][1]
+            if (len(scheduled_stops) > 1):
+                self.target_stop = scheduled_stops[1][0]
+                self.target_stop_time = scheduled_stops[1][1]
+        else:
+            self.target_stop = scheduled_stops[0][0]
+            self.target_stop_time = scheduled_stops[0][1]
+
+    def get_last_stop(self):
+        return self.last_known_stop
+
+    def get_last_stop_time(self):
+        return self.last_known_time
+
+    def get_next_stop(self):
+        return self.target_stop
+
+    def get_next_stop_time(self):
+        return self.target_stop_time
+
+    def time_between_stops(self):
+        return self.target_stop_time - self.last_known_time
+
+    def fraction_remaining(self, timestamp):
+        return float(timestamp - self.last_known_time) / float(self.time_between_stops())
